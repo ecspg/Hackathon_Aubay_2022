5088ffc13b2bcc0ad7f6055157df3fb3c972e992
Daniel Galvão <galvaoazevedod@gmail.com>
2022-11-19 12:15:00 +0000

    ChannelSetting creation

---
+++ 03 - Backend/aubilousTouch/src/main/java/com/hackaton/aubilousTouch/model/ChannelSetting.java
@@ -0,0 +1,76 @@
+package com.hackaton.aubilousTouch.model;
+
+import java.time.LocalDateTime;
+
+public class ChannelSetting {
+
+    private Integer id;
+
+    private String hostname;
+
+    private ChannelType channelType;
+
+    private String key;
+
+    private String secret;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateDeleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public ChannelType getChannelType() {
+        return channelType;
+    }
+
+    public void setChannelType(ChannelType channelType) {
+        this.channelType = channelType;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateDeleted() {
+        return dateDeleted;
+    }
+
+    public void setDateDeleted(LocalDateTime dateDeleted) {
+        this.dateDeleted = dateDeleted;
+    }
+}
