2645d82d52a88110ef431e76a4361379a026600d
Daniel Galvão <galvaoazevedod@gmail.com>
2022-11-19 11:43:59 +0000

    Contact creation

---
+++ 03 - Backend/aubilousTouch/src/main/java/com/hackaton/aubilousTouch/model/Contact.java
@@ -0,0 +1,96 @@
+package com.hackaton.aubilousTouch.model;
+
+import java.time.LocalDateTime;
+
+public class Contact {
+
+    private Integer id;
+
+    private String firstName;
+
+    private User userCreated;
+
+    private User userDeleted;
+
+    private String middleName;
+
+    private String surName;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateDeleted;
+
+    private String internalIdentification;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public User getUserCreated() {
+        return userCreated;
+    }
+
+    public void setUserCreated(User userCreated) {
+        this.userCreated = userCreated;
+    }
+
+    public User getUserDeleted() {
+        return userDeleted;
+    }
+
+    public void setUserDeleted(User userDeleted) {
+        this.userDeleted = userDeleted;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateDeleted() {
+        return dateDeleted;
+    }
+
+    public void setDateDeleted(LocalDateTime dateDeleted) {
+        this.dateDeleted = dateDeleted;
+    }
+
+    public String getInternalIdentification() {
+        return internalIdentification;
+    }
+
+    public void setInternalIdentification(String internalIdentification) {
+        this.internalIdentification = internalIdentification;
+    }
+}
